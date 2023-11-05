e83e5ebc9c308aa0359c61e1ef194be48f5a1a54
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-11-04 20:17:30 -0700

    first commit

---
+++ src/collections/Customers.ts
@@ -0,0 +1,27 @@
+import {CollectionConfig } from 'payload/types';
+import {selfOrAdmin} from '../access/selfOrAdmin';
+
+export const Customers:CollectionConfig ={
+    slug:'customers',
+    auth:true,
+    access:{
+        create: () => true,
+        read:selfOrAdmin,
+        update:selfOrAdmin,
+        delete:({req:{user}})=>{
+            return user?.collection === 'users';
+        }
+    },
+    fields:[
+        {
+            name:'firstName',
+            type:'text',
+            required:true,
+        },
+        {
+            name:'lastName',
+            type:'text',
+            required:true
+        }
+    ]
+}
